9cb9589a2f2116507e8e86e3ebb0281873d53828
godeastone <godeastone@kaist.ac.kr>
2024-05-30 02:53:42 +0900

    (WIP) implementing noise optimization

--- add_noise.py
+++ add_noise.py
@@ -51,22 +51,59 @@ def main(args):
     # optimizer for w
     optimizer = optim.Adam([w], lr=1e-2)
 
+    # initialzie noise vector
+    noise = torch.randn((batch_size, 1, 28, 28), requires_grad=True, device=device)
+    optimizer_noise = optim.Adam([noise], lr=1e-2)
+
     for data, _ in train_loader:
-        data = data.to(device)
-        tensor2png(data[1])
-        print(model.get_coverage(data, 1))
+        # data = data.to(device)
+        # print("##")
+        # print(data.size)
+        # tensor2png(data[1])
+        # print(model.get_coverage(data, 1))
+        # exit()
+
+        # Optimize noise iteratively
+        for _ in range(args.step_num):
+            # load data
+            data = data.to(device)
+
+            # init optimizer
+            optimizer_noise.zero_grad()
+            
+            # perturbed image
+            perturbed_data = data + noise
+
+            # apply constraints
+            perturbed_data = torch.clamp(perturbed_data, -1, 1)
+
+            # compute coverage
+            coverage = model.get_coverage(perturbed_data, 1)
+
+            # our loss function
+            loss_noise = torch.norm(noise) * args.lagrangian - coverage.mean()
+
+            # optimize
+            loss_noise.backward()
+            optimizer_noise.step()
+
+        tensor2png(perturbed_data[1])
+        print(model.get_coverage(perturbed_data, 1))
         exit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", choices=["MNIST", "CIFAR10"], default="MNIST")
     parser.add_argument("-m", "--model", choices=["MLP", "CIFAR10"], default="MLP")
-
     parser.add_argument(
         "--model-path",
         required=True,
         help="save path of model weight",
     )
+    # args for noise
+    parser.add_argument("-t", "--step-num", default=100, type=int)
+    parser.add_argument("-l", "--lagrangian", default=0.1, type=float)
+
     args = parser.parse_args()
 
     main(args)
