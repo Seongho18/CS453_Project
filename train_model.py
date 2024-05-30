b503207c2ef07674da41cd2b55879b8116e6a071
Sungwoo Jeon <j0070ak@kaist.ac.kr>
2024-05-30 20:55:26 +0900

    fix model argument

--- train_model.py
+++ train_model.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-dataset", "--dataset", choices=["MNIST", "CIFAR10"], default="MNIST")
-    parser.add_argument("-m", "--model", choices=["MLP", "CIFAR10"], default="MNIST")
+    parser.add_argument("-m", "--model", choices=["MLP", "LENET"], default="MLP")
     parser.add_argument("-e", "--epochs", type=int, default=10)
     parser.add_argument("-b", "--batch", type=int, default=64)
     parser.add_argument("-lr", "--learning-rate", type=float, default=0.001)
